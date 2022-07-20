887baf0c73d9f95ab401bda7c0363230031f0a05
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-20 19:45:15 +0300

    Started to work on basic strutcure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
