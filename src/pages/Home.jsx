4510be10cf98af39eb18c7be0ab824ec3e312f69
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-20 19:57:04 +0300

    Added shop page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Home;
