6e92a2788af16eb76e07c252208c93474ca3ec48
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-22 00:24:26 +0300

    Added scss to work scss modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
