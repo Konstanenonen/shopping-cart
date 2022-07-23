6ae5c62c527209ad18a577f23ef15fbb93cdc207
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-23 11:57:36 +0300

    Made the shop page show multiple items

---
+++ src/data/shopItems.js
@@ -0,0 +1,44 @@
+const shopItems = [
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+  {
+    name: 'Item name',
+    description: 'Item description',
+  },
+];
+
+export default shopItems;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import ShopCartBar from '../components/ShopCartBar';
 import ShopItem from '../components/ShopItem';
+import shopItems from '../data/shopItems';
 
 function Shop() {
   const [itemsAmount, setItemsAmount] = useState(0);
+  const items = shopItems;
 
   const addToCart = (amount) => {
     setItemsAmount((i) => i + amount);
   };
 
+  const itemElements = items.map((item) => (
+    <ShopItem
+      title={item.title}
+      description={item.description}
+      addToCart={addToCart}
+    />
+  ));
+
   return (
     <>
       <ShopCartBar itemsAmount={itemsAmount} />
-      <ShopItem
-        title="Example Item"
-        description="Interesting example description"
-        addToCart={addToCart}
-      />
+      <div>{itemElements}</div>
     </>
   );
 }
