4d06df33665b753dd2dc236a0e83c1d38f6b4e29
Konsta Nenonen <konstanenonen@protonmail.com>
2022-07-25 19:02:43 +0300

    Added more content and styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from '../styles/pages/Home.module.scss';
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <p>This is Home</p>
+      <h1 className={styles.title}>Home page</h1>
+      <p className={styles.text}>
+        This project was made to learn{' '}
+        <span className={styles.red}>React testing library</span> and{' '}
+        <span className={styles.red}>React Router library</span>
+      </p>
     </>
   );
 }
